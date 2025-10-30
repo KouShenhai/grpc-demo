3a868de3b8e02ac5619b0162fdf571f7625e120c
koushenhai <2413176044@qq.com>
2025-10-30 14:00:35 +0800

    feat: 1

---
+++ server-demo/src/test/java/org/laokouyun/demo/ServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.laokouyun.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
